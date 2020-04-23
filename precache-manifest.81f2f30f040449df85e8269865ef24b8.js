b294d22882f28632beb08e670388ad7fc47b614e
Keyul Patel <keyulpatel@Keyuls-MacBook-Pro.local>
2020-04-23 16:36:00 -0500

    Updates

---
+++ precache-manifest.81f2f30f040449df85e8269865ef24b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95766b6f0e9575d74520df8b23a530b9",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ac9736a1363f6e52f786",
+    "url": "/react-portfolio/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "ea47ece4e2a9b417ff01",
+    "url": "/react-portfolio/static/js/2.19d4ab88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.19d4ab88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9736a1363f6e52f786",
+    "url": "/react-portfolio/static/js/main.fea04315.chunk.js"
+  },
+  {
+    "revision": "c01136dc6b7e393f307b",
+    "url": "/react-portfolio/static/js/runtime-main.d92c7dfa.js"
+  }
+]);
