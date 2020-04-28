97d40e9688ed903f734f46771a68d723343b6ca7
Keyul Patel <keyulpatel@Keyuls-MacBook-Pro.local>
2020-04-28 00:43:04 -0500

    Updates

---
+++ precache-manifest.354c9bbb0b1be60f155f1fd7c67e9277.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bdd18055ab19c97509ac2e13992ea9",
+    "url": "/react-portfolio23/index.html"
+  },
+  {
+    "revision": "0540e91c146c31ed93df",
+    "url": "/react-portfolio23/static/js/2.62ddaeb5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio23/static/js/2.62ddaeb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0655cc71a77b791392dd",
+    "url": "/react-portfolio23/static/js/main.c93413fd.chunk.js"
+  },
+  {
+    "revision": "b7dd78ecff4c56a2e8ce",
+    "url": "/react-portfolio23/static/js/runtime-main.7de6c485.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio23/precache-manifest.86b241b18659e6f72e11fdac0c69f232.js"
+  "/react-portfolio23/precache-manifest.354c9bbb0b1be60f155f1fd7c67e9277.js"
 );
 
 self.addEventListener('message', (event) => {
