5fcd0b49469776b787ff5a3458ed5bab9e4e0e9f
Keyul Patel <keyulpatel@Keyuls-MacBook-Pro.local>
2020-05-10 14:58:08 -0500

    Updates

---
+++ precache-manifest.49652d66005cae98e00f5a2f800bc1d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2733e33509636745248cbfcb2395752d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2a6fe19e1329a89b74b",
+    "url": "/static/js/2.3e8e7cd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3e8e7cd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dff7f5ea0dcd1418166",
+    "url": "/static/js/main.b7466531.chunk.js"
+  },
+  {
+    "revision": "a0ac1140309c735bc60b",
+    "url": "/static/js/runtime-main.6be4d7d3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio23/precache-manifest.aba8f3f96a83b1bff2f181bbffc5cfa2.js"
+  "/precache-manifest.49652d66005cae98e00f5a2f800bc1d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio23/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
