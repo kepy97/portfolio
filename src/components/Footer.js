b0f8ed489acbcb8cc1cb4f3064e7d422ad1f7779
Keyul Patel <keyulpatel@Keyuls-MacBook-Pro.local>
2020-05-10 16:59:30 -0500

    new blog added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,63 +2,63 @@ import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 
 export default class Footer extends React.Component {
-    render() {
-        return (
-            <footer className="ftco-footer ftco-section">
-              <div className="container">
-                <div className="row mb-5">
-                  <div className="col-md">
-                    <div className="ftco-footer-widget mb-4">
-                      <h2 className="ftco-heading-2">About</h2>
-                      <p>I'm a software developer based in Stoughton, Wisconsin, USA. I have a passion for programming and a great experience on how to create web and mobile applications.</p>
-                      <ul className="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                        <li className="ftco-animate"><a href="https://www.linkedin.com/in/kepy97/" target="_blank"><span className="icon-linkedin" /></a></li>
-                        <li className="ftco-animate"><a href="https://github.com/kepy97" target="_blank"><span className="icon-github" /></a></li>
-                        <li className="ftco-animate"><a href="https://www.facebook.com/Kepy97" target="_blank"><span className="icon-facebook" /></a></li>
-                        <li className="ftco-animate"><a href="https://www.instagram.com/kepy97/" target="_blank"><span className="icon-instagram" /></a></li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="col-md">
-                    <div className="ftco-footer-widget mb-4 ml-md-4" id="ftco-nav">
-                      <h2 className="ftco-heading-2">Links</h2>
-                      <ul className="list-unstyled">
-                        <li><Link to={{pathname: `/index.html`, hash: "#home-section"}} ><span className="icon-long-arrow-right mr-2" />Home</Link></li>
-                        <li><Link to={{pathname: `/index.html`, hash: "#about-section"}} ><span className="icon-long-arrow-right mr-2" />About</Link></li>
-                        <li><Link to={{pathname: `/index.html`, hash: "#resume-section"}}><span className="icon-long-arrow-right mr-2" />Resume</Link></li>
-                        <li><Link to={{pathname: `/index.html`, hash: "#projects-section"}}><span className="icon-long-arrow-right mr-2" />Projects</Link></li>
-                        <li><Link to={{pathname: `/index.html`, hash: "#blog-section"}}><span className="icon-long-arrow-right mr-2" />My Blog</Link></li>
-                        <li><Link to={{pathname: `/index.html`, hash: "#contact-section"}}><span className="icon-long-arrow-right mr-2" />Contact</Link></li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="col-md">
-                    <div className="ftco-footer-widget mb-4">
-                      <h2 className="ftco-heading-2">Skills</h2>
-                      <ul className="list-unstyled">
-                        <li><span className="icon-long-arrow-right mr-2" />Web Design</li>
-                        <li><span className="icon-long-arrow-right mr-2" />Web Development</li>
-                        <li><span className="icon-long-arrow-right mr-2" />Business Strategy</li>
-                        <li><span className="icon-long-arrow-right mr-2" />Data Analysis</li>
-                        <li><span className="icon-long-arrow-right mr-2" />Software Development</li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="col-md">
-                    <div className="ftco-footer-widget mb-4">
-                      <h2 className="ftco-heading-2">Have a Questions?</h2>
-                      <div className="block-23 mb-3">
-                        <ul>
-                          <li><span className="icon icon-map-marker" /><span className="text">2216 Lake Woods Way, Stoughton, Wisconsin, USA</span></li>
-                          <li><a href="tel:+17123016140"><span className="icon icon-phone" /><span className="text">+1-712-301-6140</span></a></li>
-                          <li><a href="mailto:kvpatel@uwm.edu"><span className="icon icon-envelope" /><span className="text">kvpatel@uwm.edu</span></a></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
+  render() {
+    return (
+      <footer className="ftco-footer ftco-section">
+        <div className="container">
+          <div className="row mb-5">
+            <div className="col-md">
+              <div className="ftco-footer-widget mb-4">
+                <h2 className="ftco-heading-2">About</h2>
+                <p>I'm a software developer based in Stoughton, Wisconsin, USA. I have a passion for programming and a great experience on how to create web and mobile applications.</p>
+                <ul className="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                  <li className="ftco-animate"><a href="https://www.linkedin.com/in/kepy97/" target="_blank"><span className="icon-linkedin" /></a></li>
+                  <li className="ftco-animate"><a href="https://github.com/kepy97" target="_blank"><span className="icon-github" /></a></li>
+                  <li className="ftco-animate"><a href="https://www.facebook.com/Kepy97" target="_blank"><span className="icon-facebook" /></a></li>
+                  <li className="ftco-animate"><a href="https://www.instagram.com/kepy97/" target="_blank"><span className="icon-instagram" /></a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md">
+              <div className="ftco-footer-widget mb-4 ml-md-4" id="ftco-nav">
+                <h2 className="ftco-heading-2">Links</h2>
+                <ul className="list-unstyled">
+                  <li><Link to={{ pathname: `/index.html`, hash: "#home-section" }} ><span className="icon-long-arrow-right mr-2" />Home</Link></li>
+                  <li><Link to={{ pathname: `/index.html`, hash: "#about-section" }} ><span className="icon-long-arrow-right mr-2" />About</Link></li>
+                  <li><Link to={{ pathname: `/index.html`, hash: "#resume-section" }}><span className="icon-long-arrow-right mr-2" />Resume</Link></li>
+                  <li><Link to={{ pathname: `/index.html`, hash: "#projects-section" }}><span className="icon-long-arrow-right mr-2" />Projects</Link></li>
+                  <li><Link to={{ pathname: `/index.html`, hash: "#blog-section" }}><span className="icon-long-arrow-right mr-2" />My Blog</Link></li>
+                  <li><Link to={{ pathname: `/index.html`, hash: "#contact-section" }}><span className="icon-long-arrow-right mr-2" />Contact</Link></li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md">
+              <div className="ftco-footer-widget mb-4">
+                <h2 className="ftco-heading-2">Skills</h2>
+                <ul className="list-unstyled">
+                  <li><span className="icon-long-arrow-right mr-2" />Data Analysis</li>
+                  <li><span className="icon-long-arrow-right mr-2" />Web Design</li>
+                  <li><span className="icon-long-arrow-right mr-2" />Web Development</li>
+                  <li><span className="icon-long-arrow-right mr-2" />Mobile App Development</li>
+                  <li><span className="icon-long-arrow-right mr-2" />Software Development</li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md">
+              <div className="ftco-footer-widget mb-4">
+                <h2 className="ftco-heading-2">Have a Questions?</h2>
+                <div className="block-23 mb-3">
+                  <ul>
+                    <li><span className="icon icon-map-marker" /><span className="text">2216 Lake Woods Way, Stoughton, Wisconsin, USA</span></li>
+                    <li><a href="tel:+17123016140"><span className="icon icon-phone" /><span className="text">+1-712-301-6140</span></a></li>
+                    <li><a href="mailto:kvpatel@uwm.edu"><span className="icon icon-envelope" /><span className="text">kvpatel@uwm.edu</span></a></li>
+                  </ul>
                 </div>
               </div>
-            </footer>
-        );
-    }
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,16 +3,16 @@ import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
 
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export default class Layout extends React.Component {
-    render() {
-        return (
-        	<Router>
+	render() {
+		return (
+			<Router>
 				<Header />
-	        	<Body />
-	        	<Footer />
-        	</Router>
-        );
-    }
+				<Body />
+				<Footer />
+			</Router>
+		);
+	}
 }
