d78fa1d4d142ab0566f278ef422fda32942c3369
Keyul Patel <keyulpatel@Keyuls-MacBook-Pro.local>
2020-05-21 00:07:06 -0500

    scrolling and pagination added

--- src/components/BodyComponents/Blog/BlogIndex.js
+++ src/components/BodyComponents/Blog/BlogIndex.js
@@ -3,7 +3,20 @@ import moment from 'moment';
 import Sidebar from './Sidebar';
 import { Link } from 'react-router-dom';
 
+const MAX_POSTS = 5;
+const ref = React.createRef();
 export default class BlogIndex extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 1,
+            totalPage: Math.ceil(props.postsData.length / MAX_POSTS),
+            totalPosts: props.postsData.length,
+            startIndex: props.postsData.length,
+            endIndex: props.postsData.length - MAX_POSTS
+        };
+    }
+
     componentDidMount() {
         window.globalFunction();
         window.scrollTo({
@@ -12,8 +25,32 @@ export default class BlogIndex extends React.Component {
             behavior: 'smooth'
         });
     }
+
+    // Changing Pagination Values to handle requested posts
+    handlePaginationClick(current) {
+        if (current !== this.state.currentPage) {
+            let startIndex = this.state.totalPosts - ((current - 1) * MAX_POSTS)
+            let endIndex = (startIndex - MAX_POSTS) < 0 ? 0 : (startIndex - MAX_POSTS);
+            this.setState({
+                currentPage: current,
+                startIndex: startIndex,
+                endIndex: endIndex
+            });
+        }
+        this.handleScrolling();
+    }
+
+    handleScrolling() {
+        ref.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
     render() {
         const posts = this.props.postsData;
+        const postIndex = posts.filter(post => post.id > this.state.endIndex && post.id <= this.state.startIndex)
+
         return (
             <div>
                 <div className="hero-wrap js-fullheight">
@@ -28,12 +65,12 @@ export default class BlogIndex extends React.Component {
                         </div>
                     </div>
                 </div>
-                <section className="ftco-section">
+                <section className="ftco-section" ref={ref}>
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-8 ftco-animate" id="post-section">
                                 {
-                                    posts.map((post, i) => {
+                                    postIndex.map((post, i) => {
                                         return (
                                             <div className="blogIndexBorder" key={i}>
                                                 <h2 className="mb-3"><Link to={`/blog/${post.url}`}>{post.title}</Link></h2>
@@ -49,18 +86,19 @@ export default class BlogIndex extends React.Component {
                                     })
                                 }
                                 {/* Pagination */}
-                                {/* <nav aria-label="Page navigation example">
-                                    <ul class="pagination justify-content-center">
-                                        <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">4</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">5</a></li>
-                                        <li class="page-item disabled">
-                                            <a class="page-link" href="#">Next</a>
+                                <nav aria-label="Page navigation example">
+                                    <ul className="pagination justify-content-center cursor-pointer">
+                                        {
+                                            Array(this.state.totalPage).fill(1).map((page, index) =>
+                                                <li key={index + 1} className={index + 1 === this.state.currentPage ? 'page-item active' : 'page-item'}>
+                                                    <a className="page-link" onClick={() => this.handlePaginationClick(index + 1)}>{index + 1}</a>
+                                                </li>)
+                                        }
+                                        <li className="page-item disabled">
+                                            <a className="page-link" href="#">>></a>
                                         </li>
                                     </ul>
-                                </nav> */}
+                                </nav>
                                 {/* Pagination */}
                             </div>
                             <Sidebar postData={posts} />
