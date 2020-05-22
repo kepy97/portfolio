ffec2ef9f36b244c9f84ae2c544bbf3ab9820cad
Keyul Patel <keyulpatel@Keyuls-MacBook-Pro.local>
2020-05-21 22:13:05 -0500

    new functionality: tag cloud changes

--- src/components/BodyComponents/Blog/Sidebar.js
+++ src/components/BodyComponents/Blog/Sidebar.js
@@ -5,14 +5,24 @@ import { Link } from 'react-router-dom';
 const MAX_POSTS = 5;
 export default class Sidebar extends React.Component {
     render() {
-        let posts = this.props.postData
-        posts = posts.slice(0, MAX_POSTS);
+        let posts = this.props.postData;
+        let recentPosts = posts.slice(0, MAX_POSTS);
+
         return (
             <div className="col-lg-4 sidebar ftco-animate">
+                {/* <div className="sidebar-box ftco-animate">
+                    <h3 className="heading-sidebar">Tags</h3>
+                    <div className="tagcloud">
+                        <span className="tag-cloud-link">house</span>
+                        <span className="tag-cloud-link">house</span>
+                        <span className="tag-cloud-link">house</span>
+                        <span className="tag-cloud-link">house</span>
+                    </div>
+                </div> */}
                 <div className="sidebar-box ftco-animate">
                     <h3 className="heading-sidebar">Recent Posts</h3>
                     {
-                        posts.map((post, i) => {
+                        recentPosts.map((post, i) => {
                             return (
                                 <div className="block-21 mb-4 d-flex" key={i}>
                                     <div className="blog-img mr-4 cursor-pointer" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/images/${post.image})` }} ></div>
