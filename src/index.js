8d21c5c9fd110aa9fd69bef54336b0da8af2b30b
Keyul Patel <keyulpatel@Keyuls-MacBook-Pro.local>
2020-05-27 18:55:39 -0500

    Revert "prerendering added react-snap"
    This reverts commit c9bf58528bfd8441317d1988a1449be9de92136d.

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-const rootElement = document.getElementById("root");
-
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(<React.StrictMode><App /></React.StrictMode>, rootElement);
-} else {
-  ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, rootElement);
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
