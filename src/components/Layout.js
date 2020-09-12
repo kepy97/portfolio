195a986958a9ae098fdbd530ab14483f563b6725
Keyul Patel <keyulpatel@Keyuls-MacBook-Pro.local>
2020-09-12 11:36:33 -0500

    loading image added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
+import loadingImage from "./../data/working-user.gif"
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 export default class Layout extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {loading: false};
+	}
+	componentDidMount() {
+		this.setState({loading: true});
+		setTimeout(() => {
+			this.setState({loading: false});
+		}, 5000);
+	}
+
 	render() {
 		return (
 			<Router basename={process.env.PUBLIC_URL}>
-				<Header />
-				<Body />
-				<Footer />
+				{!this.state.loading ?
+					<Fragment>
+						<Header />
+						<Body />
+						<Footer />
+					</Fragment>
+				:
+					<div class="custom-landing-sec">
+						<div class="custom-landing-image">
+							<img src={loadingImage} alt="Loading Data" />
+						</div>
+					</div> 
+				}
 			</Router>
 		);
 	}
