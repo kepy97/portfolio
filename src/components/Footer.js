76a8fdbb2d1da26c6c41bcce0bb74a668d5207e6
Keyul Patel <keyulpatel@Keyuls-MacBook-Pro.local>
2021-01-18 13:53:14 -0600

    web content updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -38,11 +38,11 @@ export default class Footer extends React.Component {
               <div className="ftco-footer-widget mb-4">
                 <h2 className="ftco-heading-2">Skills</h2>
                 <ul className="list-unstyled">
-                  <li><span className="icon-long-arrow-right mr-2" />Software Development</li>
+                  <li><span className="icon-long-arrow-right mr-2" />Full Stack Software Development</li>
                   <li><span className="icon-long-arrow-right mr-2" />Web Development</li>
-                  <li><span className="icon-long-arrow-right mr-2" />Data Analysis</li>
+                  <li><span className="icon-long-arrow-right mr-2" />Machine Learning</li>
                   <li><span className="icon-long-arrow-right mr-2" />Mobile App Development</li>
-                  <li><span className="icon-long-arrow-right mr-2" />Web Design</li>
+                  <li><span className="icon-long-arrow-right mr-2" />Data Analysis</li>
                 </ul>
               </div>
             </div>
