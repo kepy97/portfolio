9b9b99ea9898ba03bc6886667e243dde8e6ce295
Admin <admin@Admins-MacBook-Pro.local>
2023-03-12 11:43:06 -0700

    Updates

---
+++ precache-manifest.c15ba843c88e25481f3bdc2363931774.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d69479d19d233413853",
+    "url": "/static/js/main.8d69479d.chunk.js"
+  },
+  {
+    "revision": "a71631b0b9e132fd93b4",
+    "url": "/static/js/1.a71631b0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fc56d12f00aedd1c8cbbe6d12b9f11e3",
+    "url": "/index.html"
+  }
+];
